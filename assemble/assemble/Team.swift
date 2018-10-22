1ea23b94c0dfbcb99c4d7d1b802ac72a233dc0e8
Guilherme Araujo <guiaszul@gmail.com>
2018-10-22 11:30:02 -0300

    collectionView mock-up Done

--- assemble/assemble/Team.swift
+++ assemble/assemble/Team.swift
@@ -25,11 +25,37 @@ class Team{
     }
     
     func generateThunderbolts(){
-        var spider = Character(idChar: 01, nameChar: "Spider Man", descriptionChar: "hero", thumbnailChar:UIImage(imageLiteralResourceName: "spider"))
         
-        var venom = Character(idChar: 02, nameChar:"Venom", descriptionChar: "vilian", thumbnailChar: UIImage(imageLiteralResourceName: "venom"))
-        var vision = Character(idChar: 03, nameChar: "Vision", descriptionChar: "hero", thumbnailChar: UIImage(imageLiteralResourceName: "vision"))
+        self.nameTeam = "Thunderbolts"
         
+        let spider = Character(idChar: 01, nameChar: "Spider Man", descriptionChar: "hero", thumbnailChar:UIImage(imageLiteralResourceName: "spider"))
+        
+        let venom = Character(idChar: 02, nameChar:"Venom", descriptionChar: "vilian", thumbnailChar: UIImage(imageLiteralResourceName: "venom"))
+        
+        let vision = Character(idChar: 03, nameChar: "Vision", descriptionChar: "hero", thumbnailChar: UIImage(imageLiteralResourceName: "vision"))
+
+        let loki = Character(idChar: 03, nameChar: "Loki", descriptionChar: "vilian", thumbnailChar: UIImage(imageLiteralResourceName: "loki"))
+        
+        let ultron = Character(idChar: 03, nameChar: "Ultron", descriptionChar: "vilian", thumbnailChar: UIImage(imageLiteralResourceName: "ultron"))
+        
+        let scott = Character(idChar: 03, nameChar: "Scott", descriptionChar: "hero", thumbnailChar: UIImage(imageLiteralResourceName: "scott"))
+        
+        let thor = Character(idChar: 03, nameChar: "Thor", descriptionChar: "hero", thumbnailChar: UIImage(imageLiteralResourceName: "thor"))
+        
+        let marvel = Character(idChar: 03, nameChar: "Mrs. Marvel", descriptionChar: "hero", thumbnailChar: UIImage(imageLiteralResourceName: "marvel"))
+        
+        let nuncanemvi = Character(idChar: 03, nameChar: "Fulano", descriptionChar: "NPC", thumbnailChar: UIImage(imageLiteralResourceName: "nuncanemvi"))
+        
+        let s2 = Character(idChar: 03, nameChar: "Scott", descriptionChar: "hero", thumbnailChar: UIImage(imageLiteralResourceName: "scott"))
+        
+        let s3 = Character(idChar: 03, nameChar: "Thor", descriptionChar: "hero", thumbnailChar: UIImage(imageLiteralResourceName: "thor"))
+        
+        let s4 = Character(idChar: 03, nameChar: "Mrs. Marvel", descriptionChar: "hero", thumbnailChar: UIImage(imageLiteralResourceName: "marvel"))
+        
+        let s5 = Character(idChar: 03, nameChar: "Fulano", descriptionChar: "NPC", thumbnailChar: UIImage(imageLiteralResourceName: "nuncanemvi"))
+        
+        self.members += [spider, venom, loki, vision, ultron, scott, thor, marvel, nuncanemvi, s2, s3, s4, s5]
         
     }
+    
 }
--- assemble/assemble/ViewController.swift
+++ assemble/assemble/ViewController.swift
@@ -12,17 +12,26 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     
     @IBOutlet weak var hallOfChars: UICollectionView!
     
+    var epicTeam = Team(idTeam: 01)
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        <#code#>
+        epicTeam.generateThunderbolts()
+        return self.epicTeam.members.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        <#code#>
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionViewCell", for: indexPath as IndexPath) as! CharacterCollectionViewCell
+        
+        cell.characterImage.image = self.epicTeam.members[indexPath.row].thumbnailChar
+        
+        return cell
     }
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        hallOfChars.delegate = self
+        hallOfChars.dataSource = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
